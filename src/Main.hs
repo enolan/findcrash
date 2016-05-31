19e19abbac38a2d107f1b84cecf8f6f92f7c5a48
Echo Nolan <echo@echonolan.net>
2016-05-31 12:12:53 -0700

    Working first draft

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import Data.Time
+import System.Directory
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.IO
+import System.Process
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  [procname] <- getArgs
+  time <- map (\c -> if c == ':' then '_' else c) . show <$> getCurrentTime
+  let dir = "logs" </> time
+  createDirectoryIfMissing True $ dir
+  putStrLn $ "Logs being written to " ++ dir
+  go procname dir 1
+
+go :: String -> FilePath -> Int -> IO ()
+go procname dir count = if count > 200 then putStrLn "200 attempts without failure, exiting." else
+  withFile (dir </> "stdout-" ++ show count) WriteMode $ \out ->
+    withFile (dir </> "stderr-" ++ show count) WriteMode $ \err -> do
+      (_, _, _, ph) <- createProcess $ (proc procname [])
+        {std_out = UseHandle out, std_err = UseHandle err}
+      exit <- waitForProcess ph
+      let continue = do
+            putStrLn $ "No failure, attempt " ++ show count
+            go procname dir (count + 1)
+      case exit of
+        ExitSuccess -> continue
+        ExitFailure x | x < 0 -> do
+            putStrLn $ "Attempt " ++ show count ++ " failed!"
+                      | otherwise -> continue
