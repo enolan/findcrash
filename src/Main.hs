88a7c27e0e66e0358614c4fa386e8630b365084e
Echo Nolan <echo@echonolan.net>
2016-09-07 20:13:00 -0700

    Randomized trial of test failures in Idris' node backend

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,34 @@
 module Main(main) where
 
-import Data.Time
+import Data.Word
 import Path
 import Path.IO
-import System.Environment
 import System.Exit
 import System.IO
 import System.Process
+import System.Random
+import System.Random.Shuffle
 
 main :: IO ()
 main = do
-  [procname] <- getArgs
-  time <- map (\c -> if c == ':' then '_' else c) . show <$> getCurrentTime
-  timeDir <- parseRelDir time
-  let dir = $(mkRelDir "logs") </> timeDir
-  createDirIfMissing True dir
-  putStrLn $ "Logs being written to " ++ show dir
-  go procname dir 1
+  let cmd = ".stack-work/dist/x86_64-linux/Cabal-1.22.5.0/build/regression-and-feature-tests/regression-and-feature-tests"
+  opts <- shuffleM $ replicate 50 False ++ replicate 50 True
+  let logs = $(mkRelDir "logs")
+      parDir = logs </> $(mkRelDir "par")
+      serDir = logs </> $(mkRelDir "ser")
+  mapM_ (createDirIfMissing True) [parDir, serDir]
+  mapM_ (\ser -> do
+            dir <- mkRandDir (if ser then serDir else parDir)
+            let params = "--node" : if ser then ["+RTS", "-N1"] else []
+            runLogged cmd params dir) opts
+
+mkRandDir :: Path b Dir -> IO (Path b Dir)
+mkRandDir base = do
+  randomW :: Word64 <- randomIO
+  randomDir <- parseRelDir $ show randomW
+  let randomDir' = base </> randomDir
+  createDir randomDir'
+  pure randomDir'
 
 runLogged :: String -> [String] -> Path b Dir -> IO ExitCode
 runLogged cmd params dir = do
@@ -25,24 +37,9 @@ runLogged cmd params dir = do
   stdErrRel <- parseRelFile "stderr"
   let stdoutpath = dir </> stdOutRel
       stderrpath = dir </> stdErrRel
+  writeFile (toFilePath $ dir </> $(mkRelFile "cmd")) $ unwords $ cmd : params
   withFile (toFilePath stdoutpath) WriteMode $ \out ->
       withFile (toFilePath stderrpath) WriteMode $ \err -> do
         (_, _, _, ph) <- createProcess $ (proc cmd params)
           {std_out = UseHandle out, std_err = UseHandle err}
         waitForProcess ph
-
-go :: String -> Path b Dir -> Int -> IO ()
-go procname dir count =
-  if count > 200
-  then putStrLn "200 attempts without failure, exiting."
-  else do
-    dirEx <- parseRelDir $ show count
-    exit <- runLogged procname [] (dir </> dirEx)
-    let continue = do
-              putStrLn $ "No failure, attempt " ++ show count
-              go procname dir (count + 1)
-    case exit of
-      ExitSuccess -> continue
-      ExitFailure x | x < 0 ->
-                        putStrLn $ "Attempt " ++ show count ++ " failed!"
-                    | otherwise -> continue
